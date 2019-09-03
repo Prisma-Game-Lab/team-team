be102f2d99a4028576e85bcc9b74baf0e542087e
arthusagain <arthus.james@hotmail.com>
2019-09-03 20:12:17 -0300

    no message

---
+++ team-team/Assets/Scripts/TestSpawnPotion/MovementControllerPlaceholder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControllerPlaceholder : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")*0.5f,0,Input.GetAxis("Vertical")*0.5f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Respawn"))
+        {
+            Destroy(other.gameObject);
+            GameController.potionCount -= 1;
+        }
+    }
+}
---
+++ team-team/Assets/Scripts/TestSpawnPotion/PotionSpawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script criado por arthus para gerenciar o aparecimento de poções no jogo
+
+public class PotionSpawner : MonoBehaviour
+{
+    //valor que gerencia a quantidade maxima de poções existentes
+    public int maxPotions;
+
+    //valores que definem os extremos da região permitida ao aparecimento das poções
+    public float minSpawnX;
+    public float maxSpawnX;
+    public float minSpawnZ;
+    public float maxSpawnZ;
+    public float SpawnY;
+
+    //array contendo obstaculos onde não é possivel o aparecimento das poções
+    public GameObject[] Obstacles;
+
+    //array contendo uma instância de cada tipo de poção diferente (ou várias, caso tenham probabilidades diferentes)
+    public GameObject[] potionVariants;
+
+    //PLACEHOLDER: variavel que armazena tamanho da poção, usado para verificar colisões durante o spawn
+    public float potionSize;
+
+    //PLACEHOLDER: armazena Transform do GameManager para usar como base para rotação inicial da poção
+    private Transform baseTransform;
+    void Start()
+    {
+        baseTransform = GetComponent<Transform>();
+        //cria o numero de poções igual ao máximo no inicio
+        while (GameController.potionCount < maxPotions)
+        {
+            spawnPotion();
+        }
+
+    }
+    
+    void Update()
+    {
+        //reestoca poções no jogo
+        if (GameController.potionCount < maxPotions)
+        {
+            spawnPotion();
+        }
+    }
+
+    void spawnPotion()
+    {
+        int chosenPotion = 0;
+        float spawnX = 0.0f;
+        float spawnZ = 0.0f;
+        do {
+            //escolhe aleatoriamente a poção que surgirá e a sua posição, dentro do limite definido
+            chosenPotion = Random.Range(0, potionVariants.Length);
+            spawnX = Random.Range(minSpawnX, maxSpawnX);
+            spawnZ = Random.Range(minSpawnZ, maxSpawnZ);
+
+        } while (Physics.CheckSphere(new Vector3(spawnX,SpawnY,spawnZ),potionSize));
+
+        Instantiate(potionVariants[chosenPotion], new Vector3(spawnX, SpawnY, spawnZ), baseTransform.rotation);
+        GameController.potionCount += 1;
+    }
+}
+
+
