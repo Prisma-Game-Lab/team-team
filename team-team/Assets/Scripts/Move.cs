5bb71ca3d45d08b84acc418858165e822a3abe86
Arthur Ozorio <arthurlozorio@globo.com>
2019-09-03 20:45:35 -0300

    Upload movimentação, arremesso e colisão

---
+++ team-team/Assets/Scripts/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float turnSpeed = 10.0f;
+    float angle;
+    Quaternion rot;
+    Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimenta jpgador
+        float moveHon = Input.GetAxisRaw("Horizontal");
+        float moveVer = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(moveHon, 0.0f, moveVer);
+        transform.position += move * Time.deltaTime * moveSpeed;
+
+        //Mantem posição
+        if (Mathf.Abs(moveHon) < 1 && Mathf.Abs(moveVer) < 1)
+            return;
+     
+        //Calcula direção
+        angle = Mathf.Atan2(moveHon, moveVer);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    
+        //Rotaciona o jogador
+        rot = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, turnSpeed);
+    }
+}
---
+++ team-team/Assets/Scripts/Throw.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    public Transform holdpoint;
+    public bool holding = false;
+    public Rigidbody rb;
+    public float throwSpeed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holding)
+        {
+            rb.transform.position = holdpoint.position;
+            rb.useGravity = false;
+            if (Input.GetKeyDown("space"))
+            { 
+                rb.velocity = new Vector3(transform.localScale.x, 0.0f, 0.0f) * throwSpeed;
+                rb.useGravity = true;
+                holding = false;
+            }
+        }
+            
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Potion") && holding == false)
+        {
+            holding = true;
+            other.gameObject.transform.SetParent(holdpoint);
+            other.gameObject.transform.position = holdpoint.position;
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
