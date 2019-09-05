731694d4bf72cc3e4fd9c79bc0a937cf81252429
Andre Mazal Krauss <amk1710@gmail.com>
2019-09-05 16:36:33 -0300

    inputmanager script

--- team-team/Assets/Scripts/InputManager.cs
+++ team-team/Assets/Scripts/InputManager.cs
@@ -7,24 +7,51 @@ using UnityEngine;
 Script para abstrair a relação entre as componentes do jogo(movimentação dos jogadores, por exemplo) e
 os controladores. Isso é positivo para que cada componente não precise se preocupar com problemas comuns do tipo trocar controle por teclado, controle desconectado etc.
 
-
-
 Autores: Krauss,
- */
+*/
 
-struct Joystick
+
+// a controller scheme is defined as the pair controller mode and index
+public struct ControllerScheme
 {
-    
+    public ControllerMode mode;
+    public uint index;
 }
+
+//controller mode can be either keyboard or joystick
+//we define a maximum of two keyboards and 4 joysticks, this beeing implemented below and in the input settings
+public enum ControllerMode{Keyboard, Joystick};
 
 public class InputManager : MonoBehaviour
 {
     
-    
-    
+    //singleton
+    static private InputManager Instance{ get; set;} 
 
+    //updated each frame, only queried once
+    private static string[] joystickNames;
 
+    //the axis we have defined in the project settings. Anything besides this will result in an error
+    /*
+    Defined axis:
+        - HorizontalL, VerticalL: the two main axis for movement. Follows the left joystick on controllers, and the asdf or direction-keys on a keyboard
+        - [TO-DO]HorizontalR, VerticalR: the same, but they track the right joystick, and nothing on a keyboard
+    
+    */
     // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            //destroy second instance, ie this
+            GameObject.Destroy(this);
+        }
+    }
+    
     void Start()
     {
         
@@ -33,12 +60,76 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        joystickNames = Input.GetJoystickNames();
+        
         Debug.Log(Input.GetJoystickNames().Length);
-        foreach (string str in Input.GetJoystickNames())
+        for (uint i = 0; i < joystickNames.Length; i++)
         {
-            Debug.Log(str);
+            ControllerScheme cs = new ControllerScheme();
+            cs.mode = ControllerMode.Joystick;
+            cs.index = i;
+            if(IsControllerConnected(cs))
+            {
+                Debug.Log(GetIdString(cs) + " h:" + GetAxis(cs, "HorizontalL") + " v:" + GetAxis(cs, "VerticalL"));
+            }
         }
         
+    }
+
+    /* a idéia é que essas funções funcionem exatamente como as equivalentes delas na classe
+    Input do Unity, mas que já tratem e abstraiam as diferenças entre controladores e teclados, e, de alguma maneira,
+    tratem a situação de quando este controle estiver desconectado
+
+    */
+
+    //retorna identicamente à Input.GetKeyDown, mas somente usando o controle especificado
+    //também retorna false se o controle estiver desconectado
+    public static bool GetKeyDown(ControllerScheme controller, string buttonName)
+    {
+        if(IsControllerConnected(controller))
+        {
+            string str = buttonName + GetIdString(controller);
+            return Input.GetKeyDown(str);
+        }
+        else return false;
+    }
+
+    //retorna identicamente à Input.GetAxis, mas somente usando o controle especificado
+    //também retorna false se o controle estiver desconectado
+    public static float GetAxis(ControllerScheme controller, string axisName)
+    {
+        if(IsControllerConnected(controller))
+        {
+            string str = axisName + GetIdString(controller);
+            return Input.GetAxis(str);
+        }
+        else return 0.0f;
+    }
+
+    //for a given controller scheme, tells if it is connected or not
+    public static bool IsControllerConnected(ControllerScheme controller)
+    {
+        if(controller.mode == ControllerMode.Keyboard)
+        {
+            //teclado está sempre conectado(será?)
+            return true;
+        }
+        else
+        {
+            //se o joystick com esse índice estiver conectado,
+            //(podemos testar se está conectado checando o nome retornado na string[] Input.GetJoystickNames)
+            if(controller.index < joystickNames.Length && joystickNames[controller.index].Length > 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+
+    private static string GetIdString(ControllerScheme controller)
+    {
+        //constrói a string identificadora para um determinado controle. Isso só vai sentido se for seguido este mesmo padrão o input settings
+        return controller.mode.ToString() + controller.index.ToString(); 
     }
 
 
