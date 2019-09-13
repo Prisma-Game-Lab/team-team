e87187cd8a201dc3ff454fee4fb8e4464afb5eb0
Andre Mazal Krauss <amk1710@gmail.com>
2019-09-13 00:00:07 -0300

    adicionar ajuste de tempo de aceleracao e desaceleracao do player

--- team-team/Assets/Scripts/Move.cs
+++ team-team/Assets/Scripts/Move.cs
@@ -8,8 +8,13 @@ public class Move : MonoBehaviour
 {
     [Header("Variáveis de customização do movimento do jogador. Ver tooltips para mais informações")]
     
-    [Tooltip("A velocidade base de movimentação do player")]
+    [Tooltip("A velocidade base(máxima) de movimentação do player")]
     public float moveSpeed = 5.0f;
+
+    [Tooltip("O tempo em segundo que o player demora de velocidade 0 para velocidade máxima")]
+    public float accelerationTime = 0.1f;
+    [Tooltip("O tempo em segundo que o player demora de velocidade máxima para velocidade 0")]
+    public float decelerationTime = 0.2f;
     [Tooltip("Determina se o jogador deve tentar rotacionar automaticamente quando é movido. Só é válido para joysticks, já que no teclado isso acontece de qualquer jeito")]
     public bool AutoRotate;
     [Tooltip("Caso ShouldAutoRotate esteja ativo, a velocidade base de rotação do jogador")]
@@ -21,10 +26,21 @@ public class Move : MonoBehaviour
     //classe boba usada para centralizar configurações de controle do player. Acessada por esta classe pq isso afeta a movimentação
     private PlayerInput playerInput;
 
+    private Vector3 move; //um vetor usado pra "passar input" da update pra fixed update
+    private float currentSpeed; //um float usado pra manter a velocidade do player, irrespectivamente de direção
+
+    private float yAcceleration; //não mexer. Usado pra SmoothDamp
+    private float baseDeceleration; 
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+
+        //calcula coeficiente de desaceleração do player
+        // aceleração = Vfinal - Vinicial / deltaTempo =>
+        //desaceleração por frame = 0 - velocidadeMaxima / tempoDesaceleração
+        baseDeceleration = (0 - moveSpeed) / decelerationTime;
+        Debug.Assert(baseDeceleration < 0.0f);
     }
 
     // Update is called once per frame
@@ -35,8 +51,27 @@ public class Move : MonoBehaviour
         float moveHon = InputManager.GetAxis(playerInput.controllerScheme, "HorizontalL");
         float moveVer = InputManager.GetAxis(playerInput.controllerScheme, "VerticalL");
         //Debug.Log(moveVer);
-        Vector3 move = new Vector3(moveHon, 0.0f, moveVer);
-        transform.position += move * Time.deltaTime * moveSpeed;
+        Vector3 newMove = new Vector3(moveHon, 0.0f, moveVer);
+
+
+        //calcula qual deve ser a velocidade do player
+        if(newMove.sqrMagnitude > 0.1f)
+        {
+            //se player está inserindo input, ele tenta acelerar, até o limite da velocidade máxima
+            move = newMove;
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, moveSpeed, ref yAcceleration, accelerationTime);
+        }
+        else
+        {
+            //senão, desacelera
+            //V = V0 + at => Vframe = V(ultimoframe) + baseDeceleration * Time.deltaTime
+            //repare que baseDeceleration já é negativo!
+            currentSpeed = Mathf.Max(0.0f, currentSpeed + baseDeceleration * Time.deltaTime);
+        }
+
+
+        //move o player
+        transform.position += move.normalized * Time.deltaTime * currentSpeed;
 
         
         //Calcula direção
@@ -54,6 +89,8 @@ public class Move : MonoBehaviour
             float h = InputManager.GetAxis(playerInput.controllerScheme, "HorizontalR");
             float v = InputManager.GetAxis(playerInput.controllerScheme, "VerticalR");
             Vector2 input = new Vector2(h,v);
+        
+
             //se o player está usando o analógico direito para se mexer, imediatamente vai para aquela orientação(por ora pelo menos)
             if(input.magnitude >= 0.6f) //botei um thrshold de 0.6 pra ele não ficar rodando a esmo e ficar sempre na direção do último input
             {
@@ -83,5 +120,10 @@ public class Move : MonoBehaviour
         }
 
 
+    }
+
+    void FixedUpdate()
+    {
+        
     }
 }
