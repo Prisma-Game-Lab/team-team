d28e76f1dd1a1777d25290b9b97cf347f9e2d5fa
Arthur Ozorio <arthurlozorio@globo.com>
2019-10-11 18:16:33 -0300

    Menu Inicial básico

---
+++ team-team/Assets/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Iniciar()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Sair()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
