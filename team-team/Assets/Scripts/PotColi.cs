a39f17361df33e491802577264f0a3878d85c87b
Andre Mazal Krauss <amk1710@gmail.com>
2019-10-18 00:42:28 -0300

    quique meio bugado

--- team-team/Assets/Scripts/PotColi.cs
+++ team-team/Assets/Scripts/PotColi.cs
@@ -29,8 +29,17 @@ public class PotColi : MonoBehaviour
     [Tooltip("Variavel que define a altura que a orbe alcança, quando em queda do tipo arco, ou o tempo antes de cair, para queda em linha")]
     public int airTime;
 
+    private Rigidbody rigidbody;
+    private Collider collider;
+    private float raycastDistance = 0.5f;
+    //armazenamos sempre qual o último objeto em que quicamos, para poder bloquear quiques duplos
+    private GameObject lastObjectBouncedOn;
+
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        lastObjectBouncedOn = null;
     }
     
     void Update()
@@ -109,6 +118,7 @@ public class PotColi : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        //K: seria válido testar aqui se a poção foi arremessada? thrown?
         if (other.gameObject.CompareTag("Target"))
         {
             //J: destroi a poção ao colidir com o caldeirão
@@ -125,10 +135,67 @@ public class PotColi : MonoBehaviour
             this.HitPlayer(pe);
             GameController.potionCount--;
         }
-        else if (thrown)
+        else if (thrown && other.gameObject.layer == LayerMask.NameToLayer("floor"))
         {
+            //destruído ao colidir com chão
             Destroy(gameObject);
             GameController.potionCount--;
+        }
+        else if(thrown && lastObjectBouncedOn != other.gameObject)
+        {
+            lastObjectBouncedOn = other.gameObject;
+            Debug.Log("quica" + lastObjectBouncedOn.ToString());
+            //quica ao colidir com outras coisas
+            //outros orbes???
+            
+            //direções em que devo checar colisões
+            int hDir = rigidbody.velocity.x > 0 ? 1 : -1;
+            int vDir = rigidbody.velocity.z > 0 ? 1 : -1;
+
+            Vector3 newVelocity = rigidbody.velocity;
+            RaycastHit hit;
+            //há 8 origens para o raycast, uma para cada canto do bound
+            Vector3[] origins = new Vector3[8];
+            float dx = collider.bounds.extents.x; float dy = collider.bounds.extents.y; float dz = collider.bounds.extents.z;
+            origins[0] = collider.bounds.center + new Vector3(dx, dy, dz);
+            origins[1] = collider.bounds.center + new Vector3(dx, dy, -dz);
+            origins[2] = collider.bounds.center + new Vector3(dx, -dy, dz);
+            origins[3] = collider.bounds.center + new Vector3(dx, -dy, -dz);
+            origins[4] = collider.bounds.center + new Vector3(-dx, dy, dz);
+            origins[5] = collider.bounds.center + new Vector3(-dx, dy, -dz);
+            origins[6] = collider.bounds.center + new Vector3(-dx, -dy, dz);
+            origins[7] = collider.bounds.center + new Vector3(-dx, -dy, -dz);
+            
+            //há colisores nas direções de movimento? se sim, inverto minha velocidade nessa direção -> quico
+            //horizontal, ou x
+            foreach(Vector3 origin in origins)
+            {
+                Debug.DrawRay(origin, Vector3.right * hDir * raycastDistance, Color.red, 2);
+                if(other.Raycast(new Ray(this.transform.position, Vector3.right * hDir), out hit, raycastDistance))
+                {
+                    newVelocity.x = -newVelocity.x;
+                    Debug.Log("Invert x!");
+                    break;
+                }
+            }
+            
+            //"vertical", ou z
+            foreach(Vector3 origin in origins)
+            {
+                Debug.DrawRay(origin, Vector3.forward * vDir * raycastDistance, Color.blue, 2);
+                if(other.Raycast(new Ray(this.transform.position, Vector3.forward * vDir), out hit, raycastDistance))
+                {
+                    newVelocity.z = -newVelocity.z;
+                    Debug.Log("Invert z!");
+                    break;
+                }
+            }
+            
+
+            rigidbody.velocity = newVelocity;
+
+            //verificar se é necessário botar algum tipo de cooldown no quique
+
         }
         
     }
